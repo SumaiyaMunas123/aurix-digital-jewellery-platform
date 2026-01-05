3a850de1a1cfdaa216932b460623266aecb722d0
Sanu202 <jisanujayalath@gmail.com>
2026-01-05 22:34:59 +0530

    Sample footer

---
+++ admin-panel/login-page/src/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer(){
+    return(
+        <footer>
+            <p> © 2026 Aurix. All rights reserved.</p>
+        </footer>
+
+    );
+}
+export default Footer
