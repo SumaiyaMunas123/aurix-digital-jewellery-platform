55fe791ad0db4d9a5747f02d1a7d4e1dc7c880f6
Sanu202 <jisanujayalath@gmail.com>
2026-01-22 00:05:29 +0530

    Admin login created for web page

--- admin-panel/login-page/src/main.jsx
+++ admin-panel/login-page/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Footer from './Footer'
+import AdminFrontend from './adminFrontend'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Footer/>
+    <AdminFrontend/>
   </StrictMode>
 )
