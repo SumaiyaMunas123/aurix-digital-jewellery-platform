d67e8f8cf308ce5d3b27eacd717dd9d1903c8537
theachchu <achchu1118@gmail.com>
2026-03-12 17:52:21 +0530

    removed the old frontend and added new

--- frontend/lib/features/jeweller/shell/jeweller_app_shell.dart
+++ frontend/lib/features/jeweller/shell/jeweller_app_shell.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import '../widgets/jeweller_bottom_nav.dart';
 
-class JewellerAppShell extends StatelessWidget {
+class JewellerAppShell extends StatefulWidget {
   const JewellerAppShell({super.key});
 
   @override
+  State<JewellerAppShell> createState() => _JewellerAppShellState();
+}
+
+class _JewellerAppShellState extends State<JewellerAppShell> {
+  int _index = 0;
+
+  final _pages = const [
+    Center(child: Text("Dashboard")),
+    Center(child: Text("Products")),
+    Center(child: Text("Messages")),
+    Center(child: Text("Profile")),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Jeweller App Shell\n(We’ll build this later)",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.w800),
-        ),
+    return Scaffold(
+      body: _pages[_index],
+      bottomNavigationBar: JewellerBottomNav(
+        index: _index,
+        onTap: (i) {
+          setState(() {
+            _index = i;
+          });
+        },
       ),
     );
   }
--- jeweller-frontend/android/app/src/main/kotlin/com/example/jeweller_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jeweller_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
