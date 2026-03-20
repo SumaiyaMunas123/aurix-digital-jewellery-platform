a8235945c1688c3f6dcc2f2c2eacf1172101f774
chathuherath621-pixel <chathuherath621@gmail.com>
2026-03-20 08:37:57 +0530

    frontend: wire customer nav and product detail actions

--- frontend/lib/features/customer/customer_shell/customer_shell_screen.dart
+++ frontend/lib/features/customer/customer_shell/customer_shell_screen.dart
@@ -28,8 +28,8 @@ class _CustomerShellScreenState extends State<CustomerShellScreen> {
     CustomerHomeScreen(),
     CustomerSearchScreen(),
     AiStudioScreen(),
-    WishlistScreen(),
     ChatListScreen(),
+    WishlistScreen(),
   ];
 
   void _setIndex(int i) {
